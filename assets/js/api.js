da7b5747a5eb7f966db71ca6009acf1f77ddca8a
Ge0h <george.walker.wme@gmail.com>
2022-10-18 10:05:06 +0100

    Main page article covers complete incorporating NASA API

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,12 +1,17 @@
-fetch(`https://api.nasa.gov/planetary/apod?api_key=Watj9Kgy2p9T6xXiqp7beRkupgcACwu0K4bu6dHs&count=12`)
+fetch(`https://api.nasa.gov/planetary/apod?api_key=Watj9Kgy2p9T6xXiqp7beRkupgcACwu0K4bu6dHs&count=3`)
     .then(res => res.json())
     .then(data => {
         console.log(data)
         data.forEach((obj, i) => {
-            console.log(document.querySelector(`#description${i}`))
-            document.querySelector(`#description${i}`).textContent = obj.explanation
-            document.querySelector(`#image${i}`).src = obj.hdurl
-            document.querySelector(`#link${i}`).href = obj.hdurl
+            document.querySelector(`#title${i}`).textContent = obj.title
+            document.querySelector(`#date${i}`).textContent = obj.date
+            document.querySelector(`#date${i}`).datetime = obj.date
+            document.querySelector(`#explanation${i}`).textContent = obj.explanation
+            if (obj.media_type === "image") {
+                document.querySelector(`#image${i}`).src = obj.hdurl
+            } else if (obj.media_type === "video") {
+                document.querySelector(`#video${i}`).src = obj.url
+            }
         })
     })
     .catch(err => console.log(`Error: ${err}`))
