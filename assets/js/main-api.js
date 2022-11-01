8f5936e66429835c9b9ac8ab53c227e1503526be
George Walker <george.walker.wme@gmail.com>
2022-11-01 16:08:18 +0000

    Update to api

--- assets/js/main-api.js
+++ assets/js/main-api.js
@@ -1,36 +1,34 @@
 
 // Load main page articles
-if (document.URL.includes('index.html')) {
-    fetch(`https://api.nasa.gov/planetary/apod?api_key=Watj9Kgy2p9T6xXiqp7beRkupgcACwu0K4bu6dHs&count=7`)
-    .then(res => res.json())
-    .then(data => {
-        singlePageData = data;
-        data.forEach((obj, i) => {
-            if (i < 3) {
-                document.querySelector(`#title${i}`).textContent = obj.title
-                document.querySelector(`#date${i}`).textContent = obj.date
-                document.querySelector(`#date${i}`).datetime = obj.date
-                document.querySelector(`#explanation${i}`).textContent = obj.explanation
-                if (obj.media_type === "image") {
-                    document.querySelector(`#image${i}`).src = obj.hdurl;
-                    document.querySelector(`#video${i}`).style.display = 'none';
-                    document.querySelector(`#image${i}`).src = obj.hdurl;
-                    document.querySelector(`#image${i}`).style.display = 'block';
-                } else if (obj.media_type === "video") {
-                    document.querySelector(`#video${i}`).src = obj.url;
-                    document.querySelector(`#image${i}`).style.display = 'none';
-                    document.querySelector(`#video${i}`).src = obj.url;
-                    document.querySelector(`#video${i}`).style.display = 'block';
-                }
-            } else if (i < 7) {
-                document.querySelector(`#title${i}`).textContent = obj.title;
-                document.querySelector(`#date${i}`).textContent = obj.date;
+fetch(`https://api.nasa.gov/planetary/apod?api_key=Watj9Kgy2p9T6xXiqp7beRkupgcACwu0K4bu6dHs&count=7`)
+.then(res => res.json())
+.then(data => {
+    singlePageData = data;
+    data.forEach((obj, i) => {
+        if (i < 3) {
+            document.querySelector(`#title${i}`).textContent = obj.title
+            document.querySelector(`#date${i}`).textContent = obj.date
+            document.querySelector(`#date${i}`).datetime = obj.date
+            document.querySelector(`#explanation${i}`).textContent = obj.explanation
+            if (obj.media_type === "image") {
                 document.querySelector(`#image${i}`).src = obj.hdurl;
+                document.querySelector(`#video${i}`).style.display = 'none';
+                document.querySelector(`#image${i}`).src = obj.hdurl;
+                document.querySelector(`#image${i}`).style.display = 'block';
+            } else if (obj.media_type === "video") {
+                document.querySelector(`#video${i}`).src = obj.url;
+                document.querySelector(`#image${i}`).style.display = 'none';
+                document.querySelector(`#video${i}`).src = obj.url;
+                document.querySelector(`#video${i}`).style.display = 'block';
             }
-        })
+        } else if (i < 7) {
+            document.querySelector(`#title${i}`).textContent = obj.title;
+            document.querySelector(`#date${i}`).textContent = obj.date;
+            document.querySelector(`#image${i}`).src = obj.hdurl;
+        }
     })
-    .catch(err => console.log(`Error: ${err}`));
-}
+})
+.catch(err => console.log(`Error: ${err}`));
 
 
 // Get links
